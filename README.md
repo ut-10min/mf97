da240fd76300690e9119f07105aaa51a4ee70cfb
high-rubber <g-takashi120@g.ecc.u-tokyo.ac.jp>
2023-11-26 13:40:11 +0900

    Update

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# mf97 (MayFes 2024)
+# mf97 (Mayfes 2024)
 
 ## TODO list
 ### mf97 (This repository)
-* [ ] Update dates
-* [ ] Update titles
-* [ ] Update time tables
+* [ ] Update data.js (talks)
+* [ ] Update timedata.js (timetable)
+* [ ] Update timetable.js (timetable generating algorithm)
+* [ ] Update index.html
+    - Event dates (year, day)
+    - Zoom webinar registration form
 
 ### ut-10min.github.io
 * [ ] Update the latest event
 * [ ] Update Formzu
-* [ ] Update webinar registration form
+* [ ] Update the webinar registration form
--- js/timetable.js
+++ js/timetable.js
@@ -1,45 +1,51 @@
 function construstTimeTable(timeTable, talksData) {
-  return Object.keys(timeTable)
-    .filter(function (k) {return timeTable[k];})
-    .sort()
-    .map(function (time) {
-      console.log(time);
-      var name = timeTable[time];
-      console.log(name);
+    return Object.keys(timeTable)
+        .filter(function (k) { return timeTable[k]; })
+        .sort()
+        .map(function (time) {
+            console.log(time);
+            var name = timeTable[time];
+            console.log(name);
 
-      var index = 0;
-      var talk = talksData.filter(function (t) { return t.name.indexOf(name) == 0; })[index];
-      console.log(talk)
-
-      if (name == "改行") {
-        return { time: "\xa0", name: "\xa0", title: "", major: ""};
-      }
-      else if (name == "座談会1") {
-        return { time: time, name: "", title: "座談会①「専門分野に興味を持ったきっかけ」「大学院を卒業した後の進路」「研究生活で楽しいこと/大変なこと」", major: ""};
-      }
-      else if (name == "座談会2") {
-        return { time: time, name: "", title: "座談会②「研究の息抜き、課外活動」", major: ""};
-      }
-      else if (name == "講演終了") {
-        return { time: time, name: "講演終了", title: "", major: ""};
-      }
-      else {
-        return { time: time, name: talk.name, title: talk.title, major: talk.affiliation };
-      }
-    });
+            var index = 0;
+            var talk = talksData.filter(function (t) { return t.name.indexOf(name) == 0; })[index];
+            console.log(talk)
+            // 何部目か判定
+            if (
+                (name == "第1部") ||
+                (name == "第2部") ||
+                (name == "第3部") ||
+                (name == "第4部")
+            ) {
+                return { time: name, name: "", title: "", major: "" };
+            }
+            // 改行
+            else if (name == "改行") {
+                return { time: "\xa0", name: "\xa0", title: "", major: "" };
+            }
+            // 休憩・座談会
+            else if (name.indexOf("休憩・座談会") == 0) {
+                return { time: time, name: "", title: name, major: "" };
+            }
+            // そのほか(複数講演がある場合など)はここに追加
+            // 講演
+            else {
+                return { time: time, name: talk.name, title: talk.title, major: talk.affiliation };
+            }
+        });
 }
 
 
 $(function () {
-  var firstDayTable  = construstTimeTable(day1, data);
-  var secondDayTable = construstTimeTable(day2, data);
-  // var thirdDayTable  = construstTimeTable(day3, data);
+    var firstDayTable = construstTimeTable(day1, data);
+    var secondDayTable = construstTimeTable(day2, data);
+    // var thirdDayTable  = construstTimeTable(day3, data);
 
-  var template = $('#template').html();
-  Mustache.parse(template);
-  var renderedFirst  = Mustache.render(template, {table: firstDayTable,  header: "5/13 (土)"});
-  var renderedSecond = Mustache.render(template, {table: secondDayTable, header: "5/14 (日)"});
-  // var renderedThird  = Mustache.render(template, {table: thirdDayTable,  header: "11/20 (日)"});
-  // $('.article-headline').html(renderedFirst + "<br />" + renderedSecond + "<br />" + renderedThird);
-  $('.article-headline').html(renderedFirst + "<br />" + renderedSecond);
+    var template = $('#template').html();
+    Mustache.parse(template);
+    var renderedFirst = Mustache.render(template, { table: firstDayTable, header: "5/13 (土)" });
+    var renderedSecond = Mustache.render(template, { table: secondDayTable, header: "5/14 (日)" });
+    // var renderedThird  = Mustache.render(template, {table: thirdDayTable,  header: "11/20 (日)"});
+    // $('.article-headline').html(renderedFirst + "<br />" + renderedSecond + "<br />" + renderedThird);
+    $('.article-headline').html(renderedFirst + "<br />" + renderedSecond);
 });
